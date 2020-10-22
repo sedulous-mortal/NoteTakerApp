33df6fb57e5f1bb8cdc0feddff586abd0ac57f62
allie Lewis <allielewis07@gmail.com>
2020-10-21 21:16:48 -0400

    modiefied server.js

---
+++ Assets/server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+//Server 
+const app = express();
+
+// Port
+const PORT = 3000;
+
+// middleware
+app.use((req ,res,next)=>{
+    console.log (req.url);
+
+    next();
+});
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+// app.use("/api", apiRoutes);
+// app.use("/", htmlRoutes);
+
+// route
+app.get('/',(req , res ) =>{
+    res.send('got you!')
+})
+
+// Listen
+app.listen(PORT,()=>{
+    console.log(`Server started on http:localhost:${PORT}`);
+})
