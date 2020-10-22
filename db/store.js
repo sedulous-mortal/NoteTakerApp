85913331310a7045fa45981d7bafb9508a1f483b
allie Lewis <allielewis07@gmail.com>
2020-10-22 16:18:25 -0400

    updated files

--- db/store.js
+++ db/store.js
@@ -1,56 +1,27 @@
-const util = require ("util");
-const fs = require("fs");
+const express = require("express")
+//Server 
+const app = express();
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
+// Port
+const PORT = 3000;
 
-class Store {
-    read() {
-    return readFileAsync("db/db.json", "utf8");
-    }
+// middleware
+app.use((req ,res,next)=>{
+    console.log (req.url);
 
-    write(note) {
-    return writeFileAsync("db/db.json", JSON.stringify(note));
-    }
+    next();
+});
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-    getNotes() {
-    return this.read().then((notes) => {
-        let parsedNotes;
+// route
+app.get('/',(req , res ) =>{
+    res.send('got you!')
+})
 
-      // If notes isn't an array or can't be turned into one, send back a new empty array
-        try {
-        parsedNotes = [].concat(JSON.parse(notes));
-        } catch (err) {
-        parsedNotes = [];
-        }
-
-        return parsedNotes;
-    });
-    }
-
-    addNote(note) {
-    const { title, text } = note;
-
-    if (!title || !text) {
-        throw new Error("Note 'title' and 'text' cannot be blank");
-    }
-
-    // Add a unique id to the note using uuid package
-    const newNote = { title, text, id: uuidv1() };
-
-    // Get all notes, add the new note, write all the updated notes, return the newNote
-    return this.getNotes()
-        .then((notes) => [...notes, newNote])
-        .then((updatedNotes) => this.write(updatedNotes))
-        .then(() => newNote);
-    }
-
-    removeNote(id) {
-    // Get all notes, remove the note with the given id, write the filtered notes
-    return this.getNotes()
-        .then((notes) => notes.filter((note) => note.id !== id))
-        .then((filteredNotes) => this.write(filteredNotes));
-    }
-}
-
-module.exports = new Store();
+// Listen
+app.listen(PORT,()=>{
+    console.log(`Server started on http:localhost:${PORT}`);
+}) 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,39 @@
 const router = require("express").Router();
 const store = require("../db/store");
-
-// GET "/api/notes"-- all notes from the database
-router.get("/notes", (req, res) => {
+const { get } = require("./htmlRoutes");
+router.get("/api/notes", function (req, res) {
     store
-    .getNotes()
-    .then((notes) => res.json(notes))
-    .catch((err) => res.status(500).json(err));
+    // this code will execute when the server receives a GET request to api/notes
+getNotes();
+    // read what is in db.json => using fs module
+    fs.readFile("../db.store", function(err, data) { 
+        
+        // Check for errors 
+        if (err) throw err; 
+        
+    })
+    // send response with data
+    router.post("/api/notes", function (req, res) {
+        const newNote = req.body;
+            // take new note and add it to db.json using fs module
+            app.get("/api/notes", function(req, res){
+                res.json(data);
+            })
+
+//     // send response that the note was saved
+app.post("/api/notes",function(req , res){
+res.json(data);
+
+})
+
 });
 
-router.post("/notes", (req, res) => {
-    store
-    .addNote(req.body)
-    .then((note) => res.json(note))
-    .catch((err) => res.status(500).json(err));
-});
-
-// DELETE "/api/notes" deletes the note 
+// DELETE "/api/notes" deletes the note with an id equal to req.params.id
 router.delete("/notes/:id", (req, res) => {
     store
-    .removeNote(req.params.id)
-    .then(() => res.json({ ok: true }))
-    .catch((err) => res.status(500).json(err));
-});
-
-module.exports = router;
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch((err) => res.status(500).json(err));
+    });
+})
+    module.exports = router;
