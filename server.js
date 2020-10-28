2e139e6e2f02c39e4e43f5e9b5e5bf15b66a75ee
allie Lewis <allielewis07@gmail.com>
2020-10-28 15:19:01 -0400

    make it better

--- server.js
+++ server.js
@@ -28,6 +28,11 @@ app.use("/", htmlRoutes);
 app.get('/',(req , res ) =>{
     res.send('got you!')
 })
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    });
 
 // Listen
 app.listen(PORT,()=>{
